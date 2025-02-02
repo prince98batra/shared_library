d4c7a41a70f6620da7b000bd31d0dd7bdb31d24d
prince98batra <prince98batra@gmail.com>
2025-02-03 01:12:08 +0530

    Update loadconfig.groovy

--- vars/loadconfig.groovy
+++ vars/loadconfig.groovy
@@ -1,20 +1,4 @@
 def call() {
-    def config = loadConfig()
-
-    if (config.KEEP_APPROVAL_STAGE)
- {
-        echo "Skipping approval as per config!"
-    }
- else {
-        pipeline {
-            stages {
-                stage('User Approval')
- {
-                    steps {
-                        input message: "Proceed with deployment?", ok: 'Approve'
-                    }
-                }
-            }
-        }
-    }
+   def config = readYaml file: 'resources/config.yml'
+    return config
 }
