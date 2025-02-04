252516c66e5cfec03f7f50b0e272d3e0ab249513
prince98batra <prince98batra@gmail.com>
2025-02-04 17:12:27 +0530

    Update clonerepo.groovy

--- vars/clonerepo.groovy
+++ vars/clonerepo.groovy
@@ -1,18 +1,13 @@
 def call(String repoUrl) {
-    script {
-        def config = loadconfig()  // Load config.yml
-        def branch = config.ENVIRONMENT ?: "main"  // Default to "main" if ENVIRONMENT is null        echo "Cloning repository from branch: ${branch}"        sh '''
-            if [ -d "shared_library" ]; then
-                echo ":white_tick: Repository already cloned. Pulling latest changes..."
-                cd shared_library
-                git reset --hard
-                git clean -fd
-                git pull origin ''' + branch + '''
-            else
-                echo ":inbox_tray: Cloning fresh repository..."
-                rm -rf shared_library
-                git clone -b ''' + branch + ''' ''' + repoUrl + '''
-            fi
-        '''
-    }
+    def config = loadconfig()  // Load config file
+    def branch = config.ENVIRONMENT == "prod" ? "main" : "develop"
+    
+    // Directory name (assumes the repo is named shared_library)
+    def dirName = 'shared_library'
+    
+    // Remove the existing directory if it exists
+    sh "rm -rf ${dirName}"
+
+    // Clone the repository based on the branch
+    sh "git clone -b ${branch} ${repoUrl} ${dirName}"
 }
